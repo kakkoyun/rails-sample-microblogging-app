acb39e290a9fa430d37e8364d2aa18cf1944f000
Kemal Akkoyun <kakkoyun@gmail.com>
2014-03-04 14:17:05 +0200

    Static Pages finished, but tests need refactoring.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,8 +1,12 @@
 require 'spec_helper'
 
 describe 'Static Pages' do
-
   describe 'Home Page' do
+    it 'should have title "Home"' do
+      visit '/static_pages/home'
+      expect(page).to have_title 'Ruby on Rails Tutorial Sample App | Home'
+    end
+
     it 'should have content "SampleApp"' do
       visit '/static_pages/home'
       expect(page).to have_content 'SampleApp'
@@ -10,6 +14,11 @@ describe 'Static Pages' do
   end
 
   describe 'Help Page' do
+    it 'should have title "Help"' do
+      visit '/static_pages/help'
+      expect(page).to have_title 'Ruby on Rails Tutorial Sample App | Help'
+    end
+
     it 'should have content "Help"' do
       visit '/static_pages/help'
       expect(page).to have_content 'Help'
@@ -17,10 +26,14 @@ describe 'Static Pages' do
   end
 
   describe 'About Page' do
+    it 'should have title "About Us"' do
+      visit '/static_pages/about'
+      expect(page).to have_title 'Ruby on Rails Tutorial Sample App | About Us'
+    end
+
     it 'should have content "About Us"' do
       visit '/static_pages/about'
       expect(page).to have_content 'About Us'
     end
   end
-
 end
