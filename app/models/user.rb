03f317628bba18bc428456a568c0b977f45deee5
Kemal Akkoyun <kakkoyun@gmail.com>
2014-03-10 13:09:47 +0200

    Extending user tests.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ActiveRecord::Base
   has_secure_password
 
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(?:\.[a-z\d\-]+)*\.[a-z]+\z/i
 
-  before_save { self.email = email.downcase }
+  before_save { self.email = email.downcase! }
 
   validates :name, presence: true,
                    length: { maximum: 30 }
