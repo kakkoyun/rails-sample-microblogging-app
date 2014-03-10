292501a732676ca64c04bc48c3b992be9bb50386
Kemal Akkoyun <kakkoyun@gmail.com>
2014-03-10 09:37:13 +0200

    User model frst tests, accidental user view change reverted.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before { @user = User.new email: 'example@example.com', name: 'example' }
+  subject { @user }
+
+  it { should respond_to :email }
+  it { should respond_to :name }
+
+  it 'should respond to email' do # More explicit
+    expect(@user).to respond_to :email
+  end
+
 end
