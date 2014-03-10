3b05892b5aaeebef49d20e60357474c3c12ea348
Kemal Akkoyun <kakkoyun@gmail.com>
2014-03-10 12:30:59 +0200

    Email index, db level uniqueness and ensure downcase in email.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
-  validates :name, presence: true, length: { maximum: 30 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  before_save { self.email = email.downcase }
+
+  validates :name, presence: true, length: { maximum: 30 }
   validates :email, presence: true, uniqueness: { case_sensitive: false },
             format: { with: VALID_EMAIL_REGEX }
 end
