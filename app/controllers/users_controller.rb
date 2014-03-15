6ddf303adf95445fa1efc872af76a175a4b6b74e
Kemal Akkoyun <kakkoyun@gmail.com>
2014-03-15 11:08:17 +0200

    Fixing things around and completing user edit/update.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,8 +13,9 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
-    if user.update_attributes(user_params)
-      # TODO
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
     else
       render 'edit'
     end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -9,9 +9,9 @@ describe 'User Pages' do
     it { should have_title(full_title('Sign Up')) }
   end
 
-  describe "signup" do
+  describe 'signup' do
     before { visit signup_path }
-    let(:submit) { "Create my Account" }
+    let(:submit) { 'Create my Account' }
 
     describe 'with invalid information' do
       it 'should not create a new user, not change count' do
@@ -64,21 +64,41 @@ describe 'User Pages' do
     it { should have_title user.name }
   end
 
-
   describe 'user edit' do
     let(:user) { FactoryGirl.create :user }
-    before { visit edit_user_path(user) }
+    before do
+      sign_in user
+      visit edit_user_path(user)
+    end
     after { user.destroy }
 
-    describe "page" do
-      it { should have_content("Update your profile") }
-      it { should have_title("Edit user") }
+    describe 'page' do
+      it { should have_content('Update your profile') }
+      it { should have_title('Edit user') }
       it { should have_link('change', href: 'http://gravatar.com/emails') }
     end
 
-    describe "with invalid information" do
-      before { click_button "Save changes" }
+    describe 'with invalid information' do
+      before { click_button 'Save changes' }
       it { should have_content('error') }
+    end
+
+    describe 'with valid information' do
+      let(:new_name)  { 'New Name' }
+      let(:new_email) { 'new@example.com' }
+      before do
+        fill_in 'Name',             with: new_name
+        fill_in 'Email',            with: new_email
+        fill_in 'Password',         with: user.password
+        fill_in 'Confirm Password', with: user.password
+        click_button 'Save changes'
+      end
+
+      it { should have_title(new_name) }
+      it { should have_selector('div.alert.alert-success') }
+      it { should have_link('Sign-out', href: signout_path) }
+      specify { expect(user.reload.name).to  eq new_name }
+      specify { expect(user.reload.email).to eq new_email }
     end
   end
 end
