b2ebd504b3e45f8413def6c488d93de9bc81efda
Kemal Akkoyun <kakkoyun@gmail.com>
2014-03-15 11:23:51 +0200

    Authorization and before_actions for UsersController.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,21 @@
+#
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :signed_in_user, only: [:edit, :update]
+
   def new
     @user = User.new
   end
 
   def show
-    @user = User.find(params[:id])
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update_attributes(user_params)
-      flash[:success] = "Profile updated"
+      flash[:success] = 'Profile updated'
       redirect_to @user
     else
       render 'edit'
@@ -25,11 +26,14 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       sign_in @user
-      flash[:success] = "Welcome to the Sample App!"
+      flash[:success] = 'Welcome to the Sample App!'
       redirect_to @user
     else
       render 'new'
     end
+  end
+
+  def destroy
   end
 
   private
@@ -37,5 +41,13 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
+    end
+
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    def signed_in_user
+      redirect_to signin_path, notice: 'Please sign in.' unless signed_in?
     end
 end
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -6,7 +6,7 @@ describe 'Authentication' do
   describe 'signin page' do
     before { visit signin_path }
 
-    it { should have_content 'Sign-in'}
+    it { should have_content 'Sign-in' }
     it { should have_title 'Sign-in' }
   end
 
@@ -19,18 +19,18 @@ describe 'Authentication' do
       it { should have_title('Sign-in') }
       it { should have_selector('div.alert.alert-danger') }
 
-      describe "after visiting another page" do
-        before { click_link "Home" }
+      describe 'after visiting another page' do
+        before { click_link 'Home' }
         it { should_not have_selector('div.alert.alert-danger') }
       end
     end
 
-    describe "with valid information" do
+    describe 'with valid information' do
       let(:user) { FactoryGirl.create(:user) }
       before do
-        fill_in "Email",    with: user.email.upcase
-        fill_in "Password", with: user.password
-        click_button "Sign-in"
+        fill_in 'Email',    with: user.email.upcase
+        fill_in 'Password', with: user.password
+        click_button 'Sign-in'
       end
 
       it { should have_title(user.name) }
@@ -39,9 +39,27 @@ describe 'Authentication' do
       it { should have_link('Sign-out',    href: signout_path) }
       it { should_not have_link('Sign-in', href: signin_path) }
 
-      describe "followed by signout" do
-        before { click_link "Sign-out" }
+      describe 'followed by signout' do
+        before { click_link 'Sign-out' }
         it { should have_link('Sign-in') }
+      end
+    end
+  end
+
+  describe 'authorization' do
+    describe 'for non-signed-in users' do
+      let(:user) { FactoryGirl.create :user }
+
+      describe 'in UsersController' do
+        describe 'visiting user edit page' do
+          before { visit edit_user_path(user) }
+          it { should have_title('Sign-in') }
+        end
+
+        describe 'submitting to edit page' do
+          before { patch user_path(user) }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
       end
     end
   end
