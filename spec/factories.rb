bb059cc24a36eb088e6f802943ffb4514aabe66f
Kemal Akkoyun <kakkoyun@gmail.com>
2014-03-15 12:45:58 +0200

    User index pages.

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,17 @@
+# FactoryGirl.define do
+#   factory :user do
+#     name     'Foo Bar Baz'
+#     email    'foo@bar.com'
+#     password 'foobarbaz'
+#     password_confirmation 'foobarbaz'
+#   end
+# end
+
 FactoryGirl.define do
   factory :user do
-    name     "Foo Bar Baz"
-    email    "foo@bar.com"
-    password "foobarbaz"
-    password_confirmation "foobarbaz"
+    sequence(:name)  { |n| "Person #{n}" }
+    sequence(:email) { |n| "person_#{n}@example.com" }
+    password 'foobar'
+    password_confirmation 'foobar'
   end
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -19,6 +19,19 @@ describe 'User Pages' do
         expect(page).to have_selector('li', text: user.name)
       end
     end
+
+    describe 'pagination' do
+      before(:all) { 30.times { FactoryGirl.create(:user) } }
+      after(:all)  { User.delete_all }
+
+      it { should have_selector('div.pagination') }
+
+      it 'should list each user' do
+        User.paginate(page: 1).each do |user|
+          expect(page).to have_selector('li', text: user.name)
+        end
+      end
+    end
   end
 
   describe 'Sign Up page' do
