7cd294ac3eaa1d02e8795cb5dba8e1c72e0b2ead
Kemal Akkoyun <kakkoyun@gmail.com>
2014-03-16 20:39:30 +0200

    Reverse relationships a bit confused.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,15 @@ class User < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(?:\.[a-z\d\-]+)*\.[a-z]+\z/i
 
   has_many :microposts, dependent: :destroy
+
   has_many :relationships, foreign_key: 'follower_id', dependent: :destroy
   has_many :followed_users, through: :relationships, source: :followed # !!!
+
+  has_many :reverse_relationships, foreign_key: 'followed_id',
+                                   class_name:  'Relationship',
+                                   dependent:   :destroy # !!!
+  has_many :followers, through: :reverse_relationships, source: :follower
+  # source omittable
 
   validates :name, presence: true,
                    length: { maximum: 30 }
